7266d3b1d1da75de0990ba1ac5e82c20b7d787c2
Exploring-glitch <deysasha262@gmail.com>
2025-11-01 16:43:09 +0530

    created homepage animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,37 @@
 import { Outlet } from '@tanstack/react-router'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const App = () => {
+
+  useGSAP(function(){
+    const tl = gsap.timeline();
+
+    tl.from('.stair', {
+      height: 0,
+      stagger: {
+        amount: -0.25
+      }
+    })
+
+    tl.to('.stair', {
+      y: '100%',
+      stagger: {
+        amount: -0.25
+      }
+    })
+
+  })
+
   return (
     <>
+      <div className='h-screen w-screen flex fixed z-10 '>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+        <div className='stair h-full w-1/5 bg-black'></div>
+      </div>
       <Outlet />
     </>
   )
