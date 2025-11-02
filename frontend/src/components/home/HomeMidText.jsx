7210c0e4f31fff2b7050592680d1309407ce116f
Exploring-glitch <deysasha262@gmail.com>
2025-11-02 19:30:11 +0530

    changes

--- frontend/src/components/home/HomeMidText.jsx
+++ frontend/src/components/home/HomeMidText.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HomeMidText = () => {
     return (
-        <div className='text-sm text-white w-[18vw] ml-[80vw] font-[font2]'>
-            <p>&emsp; &emsp; &emsp; &emsp; K72 est une agence qui pense chaque action pour nourrir la marque. Demain, dans 5 mois et dans 5 ans. On cherche la friction qui crée l’étincelle pour générer de l’émotion. Pour assurer une relation honnête, on est sans filtre, on dit ce qui doit être dit, on fait ce qui doit être fait.</p>
+        <div className='text-sm text-white w-[18.7vw] ml-[80vw] mb-[1.5vw] font-[font2]'>
+            <p>&emsp; &emsp; &emsp; &emsp; &emsp; K72 is an agency that thinks every action to nurture the brand. Tomorrow, in 5 months and in 5 years. We look for the friction that creates the spark to generate emotion. To ensure an honest relationship, we are unfiltered, we say what needs to be said, we do what needs to be done.</p>
         </div>
     )
 }
--- frontend/src/components/home/HomeTopText.jsx
+++ frontend/src/components/home/HomeTopText.jsx
@@ -3,17 +3,17 @@ import Video from './Video.jsx'
 const HomeTopText = () => {
   return (
     <div className='text-white uppercase font-[font1] text-center mt-2 '>
-      <div className='text-[9.5vw] leading-[8.5vw] flex items-center justify-center' >L'étincelle</div>
+      <div className='text-[9.5vw] leading-[8.5vw] flex items-center justify-center' >The spark</div>
 
-      <div className='text-[9.5vw] leading-[8.5vw] flex items-start justify-center mt-1' >
-        qui
-        <div className='h-[6.5vw] w-[15vw] mt-1'>
+      <div className='text-[6vw] leading-[8.5vw] flex items-start justify-center mt-1' >
+        that
+        <div className='h-[5vw] w-[12vw] mt-[1.3vw] ml-4 mr-3.5 border-2 border-[#CCFF00]'>
           <Video />
         </div>
-        génère
+        generates
       </div>
 
-      <div className='text-[9.5vw] leading-[8.5vw] flex items-center justify-center' >la créativité</div>
+      <div className='text-[9.5vw] leading-[8.5vw] flex items-center justify-center' >the creativity</div>
     </div>
   )
 }
