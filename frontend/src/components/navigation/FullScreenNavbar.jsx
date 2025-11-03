b663703f80548e9427438be8f17ae93963ddaedb
Exploring-glitch <deysasha262@gmail.com>
2025-11-03 21:02:59 +0530

    created the nav bar

--- frontend/src/components/navigation/FullScreenNavbar.jsx
+++ frontend/src/components/navigation/FullScreenNavbar.jsx
@@ -2,89 +2,116 @@ import React from 'react'
 
 const mainNavbar = () => {
   return (
-    <div className='h-screen w-screen bg-black text-white py-37 font-[font2]'>
+    <div className='h-screen w-screen bg-black text-white overflow-hidden p-2 font-[font2]'>
+      <div>
+
+      </div>
       
-      <div className='link relative border-t-[0.1vw] text-center'>
-        <h1 className='uppercase text-[7vw] leading-0 p-12 pt-14.5'>Projects</h1>
-
-        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
-          <div className='moveX flex items-center pt-2'>
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
-
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+      <div>
+        <div className='flex justify-between'>
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg" width="115" height="50" viewBox="0 0 103 44" fill='white'>
+              <path fillRule="evenodd" d="M35.1441047,8.4486911 L58.6905011,8.4486911 L58.6905011,-1.3094819e-14 L35.1441047,-1.3094819e-14 L35.1441047,8.4486911 Z M20.0019577,0.000230366492 L8.83414254,25.3433089 L18.4876971,25.3433089 L29.5733875,0.000230366492 L20.0019577,0.000230366492 Z M72.5255345,0.000691099476 L72.5255345,8.44846073 L94.3991559,8.44846073 L94.3991559,16.8932356 L72.5275991,16.8932356 L72.5275991,19.5237906 L72.5255345,19.5237906 L72.5255345,43.9274346 L102.80937,43.9274346 L102.80937,35.4798953 L80.9357483,35.4798953 L80.9357483,25.3437696 L94.3996147,25.3428482 L94.3996147,16.8953089 L102.80937,16.8953089 L102.80937,0.000691099476 L72.5255345,0.000691099476 Z M-1.30398043e-14,43.9278953 L8.78642762,43.9278953 L8.78642762,0.0057591623 L-1.30398043e-14,0.0057591623 L-1.30398043e-14,43.9278953 Z M58.6849955,8.4486911 L43.1186904,43.9274346 L52.3166592,43.9274346 L67.9877996,8.4486911 L58.6849955,8.4486911 Z M18.4688864,25.3437696 L26.7045278,43.9278953 L36.2761871,43.9278953 L28.1676325,25.3375497 L18.4688864,25.3437696 Z"></path>
+            </svg>
           </div>
-          <div className='moveX flex items-center  pt-2'>
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
 
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+          <div className='relative h-20 w-20 cursor-pointer'>
+            <div className='absolute h-[7.3vw] w-[0.2vw] -rotate-45 origin-top bg-[#CCFF00]'></div>
+            <div className='absolute h-[7.3vw] w-[0.2vw] right-0 rotate-45 origin-top bg-[#CCFF00]'></div>
           </div>
         </div>
-      </div>
 
-      <div className='link relative border-t-[0.1vw] text-center '>
-        <h1 className='uppercase text-[7vw] leading-0 p-12 pt-14.5'>Agence</h1>
+        <div className='py-15'>
+          <div className='link relative border-t-[0.1vw] text-center'>
+            <h1 className='uppercase text-[7vw] leading-0 p-12 pt-14.5'>Projects</h1>
 
-        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
-          <div className='moveX flex items-center pt-2'>
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
+            <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
+              <div className='moveX flex items-center pt-2'>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
 
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+              </div>
+              <div className='moveX flex items-center  pt-2'>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
+
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+              </div>
+            </div>
           </div>
-          <div className='moveX flex items-center  pt-2'>
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
 
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+          <div className='link relative border-t-[0.1vw] text-center '>
+            <h1 className='uppercase text-[7vw] leading-0 p-12 pt-14.5'>Agence</h1>
+
+            <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
+              <div className='moveX flex items-center pt-2'>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
+
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+              </div>
+              <div className='moveX flex items-center  pt-2'>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
+
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+              </div>
+            </div>
+          </div>
+
+          <div className='link relative border-t-[0.1vw] text-center '>
+            <h1 className='uppercase text-[7vw] leading-0 p-12 pt-14.5'>Contact</h1>
+
+            <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
+              <div className='moveX flex items-center pt-2'>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
+
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+              </div>
+              <div className='moveX flex items-center  pt-2'>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
+
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+              </div>
+            </div>
+          </div>
+
+          <div className='link relative border-y-[0.1vw] text-center '>
+            <h1 className='uppercase text-[7vw] leading-0 p-12 pt-14.5'>Blogue</h1>
+
+            <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
+              <div className='moveX flex items-center pt-2'>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
+
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+              </div>
+              <div className='moveX flex items-center  pt-2'>
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
+
+                <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
+                <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className='link relative border-t-[0.1vw] text-center '>
-        <h1 className='uppercase text-[7vw] leading-0 p-12 pt-14.5'>Contact</h1>
-
-        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
-          <div className='moveX flex items-center pt-2'>
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
-
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
-          </div>
-          <div className='moveX flex items-center  pt-2'>
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
-
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
-          </div>
-        </div>
-      </div>
-
-      <div className='link relative border-y-[0.1vw] text-center '>
-        <h1 className='uppercase text-[7vw] leading-0 p-12 pt-14.5'>Blogue</h1>
-
-        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
-          <div className='moveX flex items-center pt-2'>
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
-
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
-          </div>
-          <div className='moveX flex items-center  pt-2'>
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
-
-            <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
-            <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/PJC/Thumbnails/PJC_SiteK72_Thumbnail_640x290-640x290.jpg" alt=""></img>
-          </div>
+        <div className='flex justify-center mt-15 gap-3 uppercase font-[font1] '>
+          <h6 className='text-xs hover:text-[#CCFF00] hover:duration-100'>Privacy Policy</h6>
+          <h6 className='text-xs hover:text-[#CCFF00] hover:duration-100'>Privacy Notice</h6>
+          <h6 className='text-xs hover:text-[#CCFF00] hover:duration-100'>Ethics Report</h6>
+          <h6 className='text-xs hover:text-[#CCFF00] hover:duration-100'>Concent Options</h6>
         </div>
       </div>
     </div>
