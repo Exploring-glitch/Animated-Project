519427de2b1a3c3c41890949bfc52093d5877e05
Exploring-glitch <deysasha262@gmail.com>
2025-11-03 19:45:46 +0530

    created the basic navbar animation

--- frontend/src/components/navigation/FullScreenNavbar.jsx
+++ frontend/src/components/navigation/FullScreenNavbar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const mainNavbar = () => {
   return (
-    <div className='h-screen w-screen bg-amber-950 text-white py-37 font-[font2]'>
+    <div className='h-screen w-screen bg-black text-white py-37 font-[font2]'>
       
-      <div className='link relative border-t-2 text-center '>
+      <div className='link relative border-t-[0.1vw] text-center'>
         <h1 className='uppercase text-8xl p-2'>Projects</h1>
 
-        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black'>
+        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
           <div className='moveX flex items-center pt-2'>
             <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
             <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
@@ -25,10 +25,10 @@ const mainNavbar = () => {
         </div>
       </div>
 
-      <div className='link relative border-t-2 text-center '>
+      <div className='link relative border-t-[0.1vw] text-center '>
         <h1 className='uppercase text-8xl p-2'>Agence</h1>
 
-        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black'>
+        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
           <div className='moveX flex items-center pt-2'>
             <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
             <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
@@ -46,10 +46,10 @@ const mainNavbar = () => {
         </div>
       </div>
 
-      <div className='link relative border-t-2 text-center '>
+      <div className='link relative border-t-[0.1vw] text-center '>
         <h1 className='uppercase text-8xl p-2'>Contact</h1>
 
-        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black'>
+        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
           <div className='moveX flex items-center pt-2'>
             <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
             <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
@@ -67,10 +67,10 @@ const mainNavbar = () => {
         </div>
       </div>
 
-      <div className='link relative border-t-2 text-center '>
+      <div className='link relative border-y-[0.1vw] text-center '>
         <h1 className='uppercase text-8xl p-2'>Blogue</h1>
 
-        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black'>
+        <div className='move_link absolute top-0 flex bg-[#CCFF00] text-black h-full'>
           <div className='moveX flex items-center pt-2'>
             <h2 className='uppercase text-8xl text-center text whitespace-nowrap'>See the projects</h2>
             <img className='shrink-0 w-50 h-18 mb-3 ml-5 mr-5 object-cover rounded-full' src="https://k72.ca/uploads/caseStudies/WIDESCAPE/WS---K72.ca---MenuThumbnail-640x290.jpg" alt=""></img>
