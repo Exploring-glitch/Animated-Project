795d6911df76f7e23c2043753c0d1e50d0a53c7b
Exploring-glitch <deysasha262@gmail.com>
2025-11-03 19:34:22 +0530

    creating the full screen nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import { Outlet } from '@tanstack/react-router'
 import Stairs from './components/common/Stairs'
 import Navbar from './components/navigation/Navbar'
+import FullScreenNavbar from './components/navigation/FullScreenNavbar'
 
 const App = () => {
 
   return (
     <>
-      <Stairs />
-      <Navbar />
-      <Outlet />
+      <FullScreenNavbar />
     </>
   )
 }
 
 export default App
+
+{/*<Stairs />
+  <Navbar />
+<Outlet />*/}
