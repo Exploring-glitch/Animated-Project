12ba2e2115a81101ad16307696a68cb8efc702ed
Exploring-glitch <deysasha262@gmail.com>
2025-11-06 21:47:53 +0530

    added the image animation

--- frontend/src/components/projects/ProjectCard.jsx
+++ frontend/src/components/projects/ProjectCard.jsx
@@ -1,7 +1,7 @@
 
 const ProjectCard = (props) => {
   return (
-    <div className='w-full h-[25vw] flex gap-2 mb-2'>
+    <>
       <div className='group w-1/2 h-full relative hover:rounded-[3.5vw] transition-all overflow-hidden'>
         <img className='h-full w-full object-cover object-top' src={props.img1} alt="" />
         <div className='opacity-0 transition-opacity group-hover:opacity-100 absolute top-0 bg-black/30 h-full w-full flex justify-center items-center'>
@@ -15,7 +15,7 @@ const ProjectCard = (props) => {
           <h2 className='text-white font-[font2] text-6xl uppercase border-4 rounded-full px-5 pt-0.5'>See The Project</h2>
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
